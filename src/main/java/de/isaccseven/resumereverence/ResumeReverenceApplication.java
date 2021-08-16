3f2e61af466000dbe8bbdbfbff20b102494858d4
Isaccseven <luca-henn@t-online.de>
2021-08-16 16:38:26 +0200

    Initial commit

---
+++ src/main/java/de/isaccseven/resumereverence/ResumeReverenceApplication.java
@@ -0,0 +1,13 @@
+package de.isaccseven.resumereverence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeReverenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResumeReverenceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/isaccseven/resumereverence/ResumeReverenceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.isaccseven.resumereverence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeReverenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
